0a15bbb0c36cb36e2beb2b08f416b9ae6a594ff4
Bassel <basselkanso82@gmail.com>
2023-02-15 17:03:52 +0200

    something

--- config/database.js
+++ config/database.js
@@ -1,11 +1,33 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
+const sqlite = {
+  client: "sqlite",
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
+    filename: path.join(
+      __dirname,
+      "..",
+      process.env.DATABASE_FILENAME || ".tmp/data.db"
+    ),
   },
-});
+  useNullAsDefault: true,
+};
+
+const postgres = {
+  client: "postgres",
+  connection: {
+    database: process.env.database || "strapi",
+    user: process.env.user || "strapi",
+    password: process.env.password || "strapi",
+    port: process.env.db_port || 5432,
+    host: process.env.host || "localhost",
+  },
+};
+
+const db = {
+  sqlite,
+  postgres,
+};
+
+module.exports = {
+  connection: process.env.DB ? db[process.env.DB] || db.sqlite : db.sqlite,
+};
