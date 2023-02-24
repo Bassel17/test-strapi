8ab4b9cbb5e9a54d10d5a78ad2a9e285a211de6e
Bassel <basselkanso82@gmail.com>
2023-02-24 17:00:41 +0200

    experimetnal version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 
-const { faker } = require('@faker-js/faker')
-const axios = require('axios')
+const { faker } = require("@faker-js/faker");
+const axios = require("axios");
 const { Readable } = require("stream");
-
 
 module.exports = {
   /**
@@ -15,43 +14,40 @@ module.exports = {
   register({ strapi }) {},
 
   async bootstrap({ strapi }) {
-
-    // for (let index = 0; index < 500; index++) {
-    //   console.log(index)
-    //   const config = strapi.config.get('plugin.upload');
-    //   const file = await axios.get(faker.image.abstract(), {
-    //     responseType: 'arraybuffer',
-    //   });
-    //   const photo = {
-    //     name: faker.word.noun()
-    //   }
-    //   const entity = {
-    //     name: `${photo.name}`,
-    //     hash: `${photo.name}`,
-    //     ext: '.jpg',
-    //     mime: file.headers['content-type'],
-    //     size: file.headers['content-length'],
-    //     provider: config.provider,
-    //     tmpWorkingDirectory: './public/uploads',
-    //     getStream: () => Readable.from(file.data),
-    //   };
-    //   await strapi.plugin('upload').service('upload').uploadImage(entity)
-    //   const media = await strapi
-    //     .query('plugin::upload.file')
-    //     .create({ data: entity });
-
+    // for (let index = 0; index < 10000; index++) {
+    //   // const config = strapi.config.get("plugin.upload");
+    //   // const file = await axios.get(faker.image.abstract(), {
+    //   //   responseType: "arraybuffer",
+    //   // });
+    //   // const photo = {
+    //   //   name: faker.word.noun(),
+    //   // };
+    //   // const entity = {
+    //   //   name: `${photo.name}`,
+    //   //   hash: `${photo.name}`,
+    //   //   ext: ".jpg",
+    //   //   mime: file.headers["content-type"],
+    //   //   size: file.headers["content-length"],
+    //   //   provider: config.provider,
+    //   //   tmpWorkingDirectory: "./public/uploads",
+    //   //   getStream: () => Readable.from(file.data),
+    //   // };
+    //   // await strapi.plugin("upload").service("upload").uploadImage(entity);
+    //   // const media = await strapi
+    //   //   .query("plugin::upload.file")
+    //   //   .create({ data: entity });
     //   try {
-    //     await strapi.entityService.create('api::article.article', {
+    //     // await strapi.query("api::article.article").deleteMany({});
+    //     await strapi.entityService.create("api::article.article", {
     //       data: {
     //         title: faker.word.noun(),
     //         content: faker.lorem.paragraph(),
-    //         image: media,
     //         publicationState: "Published",
     //         publishedAt: "2023-01-23T17:10:46.984Z",
     //       },
     //     });
     //   } catch (e) {
-    //     console.log(e.details)
+    //     console.log(e.details);
     //   }
     // }
   },
