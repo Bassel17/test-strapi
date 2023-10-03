a8d4c76bf668ebcb1dde65172670ac3b49ad9721
Bassel <basselkanso82@gmail.com>
2023-10-03 10:37:04 +0300

    upgrade to expermintal version

--- src/index.js
+++ src/index.js
@@ -15,33 +15,33 @@ module.exports = {
 
   async bootstrap({ strapi }) {
     // for (let index = 0; index < 1000; index++) {
-    //   const config = strapi.config.get("plugin.upload");
-    //   const file = await axios.get(faker.image.abstract(), {
-    //     responseType: "arraybuffer",
-    //   });
-    //   const photo = {
-    //     name: faker.word.noun(),
-    //   };
-    //   const entity = {
-    //     name: `${photo.name}`,
-    //     hash: `${photo.name}`,
-    //     ext: ".jpg",
-    //     mime: file.headers["content-type"],
-    //     size: file.headers["content-length"],
-    //     provider: config.provider,
-    //     tmpWorkingDirectory: "./public/uploads",
-    //     getStream: () => Readable.from(file.data),
-    //   };
-    //   await strapi.plugin("upload").service("upload").uploadImage(entity);
-    //   const media = await strapi
-    //     .query("plugin::upload.file")
-    //     .create({ data: entity });
+    // const config = strapi.config.get("plugin.upload");
+    // const file = await axios.get(faker.image.abstract(), {
+    //   responseType: "arraybuffer",
+    // });
+    // const photo = {
+    //   name: faker.word.noun(),
+    // };
+    // const entity = {
+    //   name: `${photo.name}`,
+    //   hash: `${photo.name}`,
+    //   ext: ".jpg",
+    //   mime: file.headers["content-type"],
+    //   size: file.headers["content-length"],
+    //   provider: config.provider,
+    //   tmpWorkingDirectory: "./public/uploads",
+    //   getStream: () => Readable.from(file.data),
+    // };
+    // await strapi.plugin("upload").service("upload").uploadImage(entity);
+    // const media = await strapi
+    //   .query("plugin::upload.file")
+    //   .create({ data: entity });
     //   try {
     //     await strapi.entityService.create("api::article.article", {
     //       data: {
     //         title: faker.word.noun(),
     //         content: faker.lorem.paragraph(),
-    //         image: media,
+    //         // image: media,
     //         publicationState: "Published",
     //         publishedAt: "2023-01-23T17:10:46.984Z",
     //       },
