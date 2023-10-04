edddb6fc99bf7f16229562a3cfe90997b74742e3
Bassel <basselkanso82@gmail.com>
2023-10-04 11:48:04 +0300

    testing with aws uploads

--- src/index.js
+++ src/index.js
@@ -14,28 +14,28 @@ module.exports = {
   register({ strapi }) {},
 
   async bootstrap({ strapi }) {
-    // for (let index = 0; index < 1000; index++) {
-    // const config = strapi.config.get("plugin.upload");
-    // const file = await axios.get(faker.image.abstract(), {
-    //   responseType: "arraybuffer",
-    // });
-    // const photo = {
-    //   name: faker.word.noun(),
-    // };
-    // const entity = {
-    //   name: `${photo.name}`,
-    //   hash: `${photo.name}`,
-    //   ext: ".jpg",
-    //   mime: file.headers["content-type"],
-    //   size: file.headers["content-length"],
-    //   provider: config.provider,
-    //   tmpWorkingDirectory: "./public/uploads",
-    //   getStream: () => Readable.from(file.data),
-    // };
-    // await strapi.plugin("upload").service("upload").uploadImage(entity);
-    // const media = await strapi
-    //   .query("plugin::upload.file")
-    //   .create({ data: entity });
+    // for (let index = 0; index < 100000; index++) {
+    //   // const config = strapi.config.get("plugin.upload");
+    //   // const file = await axios.get(faker.image.abstract(), {
+    //   //   responseType: "arraybuffer",
+    //   // });
+    //   // const photo = {
+    //   //   name: faker.word.noun(),
+    //   // };
+    //   // const entity = {
+    //   //   name: `${photo.name}`,
+    //   //   hash: `${photo.name}`,
+    //   //   ext: ".jpg",
+    //   //   mime: file.headers["content-type"],
+    //   //   size: file.headers["content-length"],
+    //   //   provider: config.provider,
+    //   //   tmpWorkingDirectory: "./public/uploads",
+    //   //   getStream: () => Readable.from(file.data),
+    //   // };
+    //   // await strapi.plugin("upload").service("upload").uploadImage(entity);
+    //   // const media = await strapi
+    //   //   .query("plugin::upload.file")
+    //   //   .create({ data: entity });
     //   try {
     //     await strapi.entityService.create("api::article.article", {
     //       data: {
